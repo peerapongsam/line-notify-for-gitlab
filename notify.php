4350f889d3500f1c5d1a83c53f84b388ef40d377
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2019-02-25 11:03:11 +0700

    Change body field

--- notify.php
+++ notify.php
@@ -117,7 +117,7 @@ function send_notify($message, $token) {
 	  CURLOPT_TIMEOUT => 30,
 	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => "POST",
-	  CURLOPT_POSTFIELDS => "message=$message",
+	  CURLOPT_POSTFIELDS => "content=$message",
 	  CURLOPT_HTTPHEADER => array(
 		  "Authorization: Bearer $token",
 		  "Content-Type: application/x-www-form-urlencoded"
@@ -128,8 +128,8 @@ function send_notify($message, $token) {
 	$err = curl_error($curl);
 	curl_close($curl);
 	if ($err) {
-	  echo "cURL Error #:" . $err;
+	  return "cURL Error #:" . $err;
 	} else {
-	  echo $response;
+	  return $response;
 	}
 }
